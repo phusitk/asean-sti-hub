0b913462616732fe448200dd5cda749d51cdbcd1
Phusit Konsurin <30390643+phusitk@users.noreply.github.com>
2025-09-23 15:49:51 +0700

    updated Content discovery

--- app.js
+++ app.js
@@ -86,6 +86,7 @@ const views = {
   directory: document.getElementById('view-directory'),
   map: document.getElementById('view-map'),
   network: document.getElementById('view-network'),
+  'content-discovery': document.getElementById('view-content-discovery'),
   admin: document.getElementById('view-admin')
 };
 tabs.forEach(t=>{
@@ -157,6 +158,7 @@ function ensureLeaflet(){
     });
     
     // ตั้งขอบเขตสูงสุดที่แสดงได้
+    
     const maxBounds = L.latLngBounds(
       L.latLng(-15, 85),   // Southwest 
       L.latLng(30, 145)    // Northeast
